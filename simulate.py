b0aeded910948bccfbb4e911614453a0f8ea05dc
XinranMiao <69163928+XinranMiao@users.noreply.github.com>
2023-04-12 17:20:30 -0500

    reprodice fig1 & 2

---
+++ simulate.py
@@ -0,0 +1,23 @@
+import numpy as np
+from scipy.stats import bernoulli
+def find_j_eigenvec(values, vectors, j = 1):
+    """
+    Find (eigenvalue, eigenvector) for the j-th largest eigenvalue (in absolute value)
+    """
+    n = len(values)
+    sort_index = np.argsort(abs(values), axis = 0)
+    val = values[sort_index[n - j]]
+    vec = vectors[:, sort_index[n - j]]
+    
+    return val, vec
+
+def conn2adj(B):
+    """
+    Generate adjacency matrix from connectivity matrix
+    """
+    n = B.shape[0]
+    A = np.zeros((n, n))
+    for i in range(n):
+        for j in range(i):
+            A[i, j] = bernoulli.rvs(size = 1, p = B[i, j])
+    return A + A.transpose()
